a17a246491c7ddf79c62dc1bd3b2aa57ac2b4f33
Josh Devins <info@joshdevins.net>
2010-05-02 18:46:34 +0200

    cleanup

--- src/main/java/net/joshdevins/rabbitmq/client/ha/HaConnectionFactory.java
+++ src/main/java/net/joshdevins/rabbitmq/client/ha/HaConnectionFactory.java
@@ -1,19 +1,68 @@
 package net.joshdevins.rabbitmq.client.ha;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-
-import net.joshdevins.rabbitmq.client.ConnectionFactory;
 
 import org.apache.commons.lang.Validate;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.InitializingBean;
 
 import com.rabbitmq.client.Address;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.ShutdownListener;
+import com.rabbitmq.client.ShutdownSignalException;
 
-public class HaConnectionFactory implements ConnectionFactory, InitializingBean {
+/**
+ * A simple {@link ConnectionFactory} proxy that further proxies any created
+ * {@link Connection} and subsequent {@link Channel}s. Sadly a dynamic proxy
+ * cannot be used since the RabbitMQ {@link ConnectionFactory} does not have an
+ * interface. As such, this class extends {@link ConnectionFactory} and
+ * overrides necessary methods.
+ * 
+ * <p>
+ * TODO: Create utility to populate some connections in the
+ * CachingConnectionFactory on startup. Should fail fast but will reconnect
+ * using this underlying.
+ * </p>
+ * 
+ * @author devins
+ */
+public class HaConnectionFactory extends ConnectionFactory
+		implements
+			InitializingBean {
+
+	protected class HaConnectionInvocationHandler implements InvocationHandler {
+
+		private final Connection target;
+
+		public HaConnectionInvocationHandler(final Connection target) {
+
+			assert target != null;
+			this.target = target;
+		}
+
+		public Object invoke(final Object proxy, final Method method,
+				final Object[] args) throws Throwable {
+
+			// TODO Auto-generated method stub
+			return null;
+		}
+	}
+
+	protected class HaConnectionShutdownListener implements ShutdownListener {
+
+		public void shutdownCompleted(final ShutdownSignalException cause) {
+			// TODO Auto-generated method stub
+
+		}
+	}
 
 	private static final Logger LOG = Logger
 			.getLogger(HaConnectionFactory.class);
@@ -31,7 +80,7 @@ public class HaConnectionFactory implements ConnectionFactory, InitializingBean 
 			final Address... addresses) {
 
 		Validate.notNull(connectionFactory, "connectionFactory is required");
-		Validate.notEmpty(addresses, "One or more addresses are required");
+		Validate.notEmpty(addresses, "one or more addresses are required");
 
 		this.connectionFactory = connectionFactory;
 
@@ -50,11 +99,8 @@ public class HaConnectionFactory implements ConnectionFactory, InitializingBean 
 	}
 
 	/**
-	 * Create a set of {@link StatefulConnection}s for each address and add
-	 * either a default {@link HaConnectionManager} as a listener or use the one
-	 * provided with the {@link #setHaConnectionManager(HaConnectionManager)}
-	 * property. Once the listener has been added, we can start the connection
-	 * attempt.
+	 * Creates a default {@link HaConnectionManager} if none was set as a
+	 * property.
 	 */
 	public void afterPropertiesSet() throws Exception {
 
@@ -63,42 +109,82 @@ public class HaConnectionFactory implements ConnectionFactory, InitializingBean 
 			haConnectionManager = new HaConnectionManager();
 		}
 
-		Set<StatefulConnection> newConnections = new HashSet<StatefulConnection>(
-				addresses.size());
-
-		for (Address address : addresses) {
-
-			if (LOG.isDebugEnabled()) {
-				LOG.debug("Creating stateful connection for: "
-						+ address.toString());
-			}
-
-			StatefulConnection connection = new StatefulConnection(
-					connectionFactory, address);
-
-			connection.addListener(haConnectionManager);
-			connection.connect();
-
-			newConnections.add(connection);
-		}
-
-		connections = Collections.unmodifiableSet(newConnections);
+		// Set<StatefulConnection> newConnections = new
+		// HashSet<StatefulConnection>(
+		// addresses.size());
+		//
+		// for (Address address : addresses) {
+		//
+		// if (LOG.isDebugEnabled()) {
+		// LOG.debug("Creating stateful connection for: "
+		// + address.toString());
+		// }
+		//
+		// StatefulConnection connection = new StatefulConnection(
+		// connectionFactory, address);
+		//
+		// connection.addListener(haConnectionManager);
+		// connection.connect();
+		//
+		// newConnections.add(connection);
+		// }
+		//
+		// connections = Collections.unmodifiableSet(newConnections);
 	}
 
 	/**
-	 * Creates and returns all stateful connections for the provided addresses.
-	 * The state of the underlying connections is "new" only if
-	 * {@link HaConnectionFactory#afterPropertiesSet()} has not been invoked. If
-	 * it as been, then they are already attempting to connect to their
-	 * destinations.
+	 * @see ConnectionFactory#newConnection(Address[])
 	 */
-	public Set<StatefulConnection> getConnections() {
-		return connections;
+	@Override
+	public Connection newConnection(final Address[] addrs) throws IOException {
+
+		return createConnectionProxy(super.newConnection(addrs));
+	}
+
+	/**
+	 * @see ConnectionFactory#newConnection(Address[], int)
+	 */
+	@Override
+	public Connection newConnection(final Address[] addrs,
+			final int maxRedirects) throws IOException {
+
+		return createConnectionProxy(super.newConnection(addrs, maxRedirects));
+	}
+
+	/**
+	 * @see ConnectionFactory#newConnection(String)
+	 */
+	@Override
+	public Connection newConnection(final String hostName) throws IOException {
+
+		return createConnectionProxy(super.newConnection(hostName));
+	}
+
+	/**
+	 * @see ConnectionFactory#newConnection(String, int)
+	 */
+	@Override
+	public Connection newConnection(final String hostName, final int portNumber)
+			throws IOException {
+
+		return createConnectionProxy(super.newConnection(hostName, portNumber));
 	}
 
 	public void setHaConnectionManager(
 			final HaConnectionManager haConnectionManager) {
 
 		this.haConnectionManager = haConnectionManager;
+	}
+
+	@SuppressWarnings("unchecked")
+	protected Connection createConnectionProxy(final Connection targetConnection) {
+
+		ClassLoader classLoader = Connection.class.getClassLoader();
+		Class[] interfaces = {Connection.class};
+		InvocationHandler invocationHandler = new HaConnectionInvocationHandler(
+				targetConnection);
+
+		return (Connection) Proxy.newProxyInstance(classLoader, interfaces,
+				invocationHandler);
 	}
 }
